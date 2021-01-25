e36e53dca0551ba8a50b117380d7a3ba9bc7c629
g511731 <ruben.canon@idemia.com>
2021-01-25 16:31:11 -0500

    Update

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,7 +1,7 @@
-def call(Map stageParams) {​​
-checkout([
-$class: 'GitSCM',
-branches: [[name: stageParams.branch ]],
-userRemoteConfigs: [[ url: stageParams.url ]]
-])
-}​​
+def call(Map stageParams) {
+    checkout([
+        $class: 'GitSCM',
+        branches: [[name:  stageParams.branch ]],
+        userRemoteConfigs: [[ url: stageParams.url ]]
+    ])
+}
